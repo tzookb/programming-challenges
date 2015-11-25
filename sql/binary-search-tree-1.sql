88b468984dd73670168d64f1d803ca8c81fce803
Tzook Bar Noy <tzookb@gmail.com>
2015-11-25 19:53:55 +0200

    sql

--- sql/binary-search-tree-1.sql
+++ sql/binary-search-tree-1.sql
@@ -1,5 +1,7 @@
+
 select t.N, (CASE WHEN t.P IS NULL THEN 'Root'
              WHEN (select count(*) from BST tn where tn.P=t.N)>0 THEN "Inner"
              ELSE "Leaf" END
             )
-from BST t;
+from BST t
+order by t.N
