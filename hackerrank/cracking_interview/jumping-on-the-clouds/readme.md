dbe830a12fc65e112cdc2a47146fb7f6708d073c
Tzook <tzookb@gmail.com>
2019-05-28 10:24:14 +0300

    jumping on the clounds

---
+++ hackerrank/cracking_interview/jumping-on-the-clouds/readme.md
@@ -0,0 +1,4 @@
+# Jumping on the Clouds - HackerRank
+
+link:
+https://www.hackerrank.com/challenges/jumping-on-the-clouds
---
+++ hackerrank/cracking_interview/jumping-on-the-clouds/sol.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	clouds := []int32{0, 0, 1, 0, 0, 1, 0}
+	res := jumpingOnClouds(clouds)
+	fmt.Println(res)
+}
+
+func jumpingOnClouds(c []int32) int32 {
+	var jumps int32
+	cidx := 0
+	eidx := len(c) - 1
+	for {
+		if cidx == eidx {
+			return jumps
+		}
+		twoStepIdx := cidx + 2
+		oneStepIdx := cidx + 1
+		if twoStepIdx <= eidx && c[twoStepIdx] == 0 {
+			jumps++
+			cidx = twoStepIdx
+			continue
+		}
+		jumps++
+		cidx = oneStepIdx
+	}
+}
